64473bf6013aab8675b7ff43fe8527d2d62910de
Kenneth Kinyanjui <kenmbugua@gmail.com>
2015-07-10 11:08:00 +0300

    added error handling to sock.py

--- sock.py
+++ sock.py
@@ -1,8 +1,12 @@
-import socket
+import socket # for sockets 
+import sys  # for exits 
 
-
-#Creating an AF_INET , SOCK socket (TCP)
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+try:
+    #Creating an AF_INET , SOCK socket (TCP)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error, msg:
+    print 'Failed to create socket. Error Code:' + str(msg[0]) + 'Error Message' + msg[1]
+    sys.exit();
+    
 print 'Sock Created' 
 
